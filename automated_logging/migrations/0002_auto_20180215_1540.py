3e60f58a2b17ceb11ec7276fe563b8fde892b3da
jchoyce <58273860+jchoyce@users.noreply.github.com>
2020-08-11 17:14:19 -0500

    Update 0002_auto_20180215_1540.py

--- automated_logging/migrations/0002_auto_20180215_1540.py
+++ automated_logging/migrations/0002_auto_20180215_1540.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Application',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('id', models.IntegerField(default=uuid.uuid4)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('name', models.CharField(max_length=255)),
