c05a53b4dfba4411d9c777f0169da8671f389bf2
Bilal Mahmoud <bilalmahmoud@posteo.net>
2020-09-20 17:51:43 +0200

    cleanup

--- logtest/testmodels/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestmodelsConfig(AppConfig):
-    name = 'testmodels'
--- logtest/testmodels/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'^$', views.save_test),
-    url(r'^d/$', views.save_test),
-]
