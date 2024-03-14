6fda4180c94c17b657c32ad458a9ca1bc386fe92
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-03-14 14:38:23 +0100

    chore: django-upgrade

--- automated_logging/__init__.py
+++ automated_logging/__init__.py
@@ -5,4 +5,3 @@ Django Automated Logging (DAL) is a package with the purpose of
 making logging in django automated and easy.
 """
 
-default_app_config = "automated_logging.apps.AutomatedloggingConfig"
